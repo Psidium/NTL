d00f3844c96ced46fe9c1238c638eb7ba5bbb591
Daniel Bertolozi <danielbertolozi@gmail.com>
2015-07-03 13:38:39 -0300

    little change, found better plugin

--- plag.py
+++ plag.py
@@ -1,10 +1,10 @@
 from docx import Document
-from google.appengine.api import search
+from pygoogle import pygoogle
 
 if __name__ == "__main__":
     if sys.args[0] == 0:
         print("Must specify file!")
-        return
+        pass # return was crashing the program
     #open the docx (and docx only)
     document = Document(sys.args[0])
     #for each paragraph on the docx
@@ -12,26 +12,21 @@ if __name__ == "__main__":
         #extract the string
         text = parag.text
         #split at whitespace
-        splitted = text.split()
+        splitted = text.split(' ', 10) # in this case, 10 words
         #send to google every 5~10 words and save the url
         #of the first Y results (parallelism preferrable, bandwidth is not a big problem,
         #the old http protocol is)
-
-    # note: check https://cloud.google.com/appengine/training/fts_intro/lesson2
-    # for instructions
-    # in this case, it will create an index document and fill it with the
-    # splitted text
-
-    index = search.Index(name='document')
-    fields = [
-            search.TextField(name=docs.Document.TEXT, value=text)
-    ]
-
-    d = search.Document(fields=fields)
-    # need to go back to work, will leave unfinished
+        ssearch = ''.join(splitted)
+        print ssearch
+        g = pygoogle(ssearch)
+        g.pages = 5
+        # g.get_result_count() will give the number of results
+        g.get_urls()   # returns a list of found urls, can be used for counting later
 
 
 
     #count the ocurrences of each URL
     #create a ratio based on the size of the document and the times an URL can appear
+
+
     #if a given URL goes beyond that ratio, it's plagiarized
